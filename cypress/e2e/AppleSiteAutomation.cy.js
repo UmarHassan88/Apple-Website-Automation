fa0ca98856c7bccc61d85e1bb4824120456f47a0
Umar Hassan <umar@finehomeint.com>
2025-04-12 09:27:30 +0400

    Till Sanity Ipad

--- cypress/e2e/AppleSiteAutomation.cy.js
+++ cypress/e2e/AppleSiteAutomation.cy.js
@@ -1,11 +1,12 @@
 describe('template spec', () => {
   it('passes', () => {
     cy.visit('https://www.apple.com/ae/?cid=aos-ae-kwgo-brand')
-    getIphone();
+    sanityIphone();
+    sanityiPad();
   })
 })
 
-function getIphone(){
+function sanityIphone(){
   cy.get('.globalnav-item-iphone > .globalnav-submenu-trigger-group > :nth-child(1) > .globalnav-link > .globalnav-link-text-container').trigger('mouseover').should('be.visible').click();
 //Compare
 cy.get('.chapternav-item-compare').click();
@@ -20,4 +21,20 @@ var selectarr2 = array1[Math.floor(Math.random() * array2.length)];
 cy.get('#selector-0').select(selectarr1);
 cy.get('#selector-1').select(selectarr2);
 
+//Buy Iphone and Scroll
+cy.get('.first-child > .button').should('contain','Buy').click();
+cy.go('back');
+cy.reload();
+//Learn More
+cy.get(':nth-child(4) > .first-child > .compare-link > .icon-copy').should('contain','Learn more').click();
+}
+
+function sanityiPad(){
+  cy.get('.globalnav-item-ipad > .globalnav-submenu-trigger-group > :nth-child(1) > .globalnav-link').should('contain','iPad').click();
+  cy.get('.ChapterNav_chapternav-items___pPsH > :nth-child(2)').contains('iPad Air').click();
+  //Scroll
+  for(let i=0;i<2;i++){
+    cy.get('#product-gallery-gallery > .paddlenav > ul > :nth-child(2)').click();
+   
+ }
 }
