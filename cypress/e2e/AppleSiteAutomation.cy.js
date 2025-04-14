e56bb8139d750da564f75c968d076d63f216a519
Umar Hassan <umar@finehomeint.com>
2025-04-14 11:25:24 +0400

    Sanity till Accessories

--- cypress/e2e/AppleSiteAutomation.cy.js
+++ cypress/e2e/AppleSiteAutomation.cy.js
@@ -1,10 +1,17 @@
 describe('template spec', () => {
   it('passes', () => {
     cy.visit('https://www.apple.com/ae/?cid=aos-ae-kwgo-brand')
-    sanityIphone();
-    sanityiPad();
+    //sanityIphone();
+    //sanityiPad();
+    //sanityEntertainment();
+    //sanityWatch();
+    sanityAccessories()
   })
 })
+const scrollProps = {
+    duration:8000,
+    easing:'swing'
+  }
 
 function sanityIphone(){
   cy.get('.globalnav-item-iphone > .globalnav-submenu-trigger-group > :nth-child(1) > .globalnav-link > .globalnav-link-text-container').trigger('mouseover').should('be.visible').click();
@@ -33,8 +40,49 @@ function sanityiPad(){
   cy.get('.globalnav-item-ipad > .globalnav-submenu-trigger-group > :nth-child(1) > .globalnav-link').should('contain','iPad').click();
   cy.get('.ChapterNav_chapternav-items___pPsH > :nth-child(2)').contains('iPad Air').click();
   //Scroll
-  for(let i=0;i<2;i++){
-    cy.get('#product-gallery-gallery > .paddlenav > ul > :nth-child(2)').click();
-   
+  for(let i=2;i>0;i--){
+    cy.get(`#product-gallery-gallery > .paddlenav > ul > :nth-child(${i}) > .paddlenav-arrow`)    
+    .should('be.visible')
+    .then($btn => {
+      if (!$btn.is(':disabled')) {
+        cy.wrap($btn).click();
+        cy.wait(2000); // give time for slide animation
+      } else {
+        cy.log(`Button ${i} is disabled, skipping click.`);
+      }
+    });
+  }
+  }
+ function sanityEntertainment(){
+  cy.get('.globalnav-item-entertainment > .globalnav-submenu-trigger-group > :nth-child(1) > .globalnav-link > .globalnav-link-text-container').should('contain','Entertainment')
+  .click();
+  cy.get('.chapternav-item-apple-books').contains('Apple Books').click();
+  
+  cy.scrollTo(0, 4000, scrollProps);
+  cy.scrollTo(4000, 0, scrollProps);
+
+  //URL Assertion
+  cy.url().should('include','apple-books');
+
  }
-}
+
+ function sanityWatch(){
+  cy.get('.globalnav-item-watch > .globalnav-submenu-trigger-group > :nth-child(1) > .globalnav-link > .globalnav-link-text-container').click();
+
+  cy.scrollTo(0, 25000, scrollProps);
+  cy.scrollTo(0, 0, scrollProps);
+  cy.get('.chapternav-item-compare').click();
+  const appleWatcharr1 = ["Apple Watch Series 10","Apple Watch Series 9","Apple Watch Series 8"];
+  const appleWatcharr2 = ["Apple Watch Series 10","Apple Watch Series 9","Apple Watch Series 8"];
+  var watch1 = appleWatcharr1[Math.floor(Math.random()* 2)];
+  var watch2 = appleWatcharr2[Math.floor(Math.random()* 2)];
+  cy.get('#selector-0').select(watch1);
+  cy.get('#selector-1').select(watch2);
+  cy.get('.button').click();
+
+ }
+ function sanityAccessories(){
+  cy.get('.globalnav-item-accessories > .globalnav-submenu-trigger-group > :nth-child(1) > .globalnav-link > .globalnav-link-text-container').click();
+  cy.scrollTo(0,200, scrollProps);
+  cy.get('.form-textbox-input').type('Apple Watch').type('{enter}');
+ }
